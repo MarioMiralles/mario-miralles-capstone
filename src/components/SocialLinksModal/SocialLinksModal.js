6241b44cbc804fd07ade71dd2184ca0a662836b6
Mario Miralles <miralles.mario@gmail.com>
2024-04-15 11:48:10 -0400

    updated the SocialLinksModal with all the styling and buttons + modal functionality

--- src/components/SocialLinksModal/SocialLinksModal.js
+++ src/components/SocialLinksModal/SocialLinksModal.js
@@ -1,16 +1,55 @@
 import '../SocialLinksModal/SocialLinksModal.scss'
 import { Link } from 'react-router-dom'
+import {
+    // Buttons
+    EmailShareButton,
+    FacebookShareButton,
+    LinkedinShareButton,
+    PinterestShareButton,
+    RedditShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    // Icons
+    EmailIcon,
+    FacebookIcon,
+    LinkedinIcon,
+    PinterestIcon,
+    RedditIcon,
+    TwitterIcon,
+    WhatsappIcon
+} from "react-share";
 
 function SocialLinksModal({ closeModal }) {
 
     return (
         <>
             <section className='social-share'>
-                <Link className="social-share__delete-close-link" to="/" onClick={closeModal}>
-                    <p className='social-share__delete-close' alt='close button'>X</p>
-                </Link>
-                <div className="news-desktop__social-row">
-                    
+                <div className='social-share__nav'>
+                    <h2 className='social-share__heading'>SHARE ==========</h2>
+                    <Link className="social-share__delete-close-link" to="/" onClick={closeModal}>
+                        <p className='social-share__delete-close' alt='close button'>X</p>
+                    </Link>
+                </div>
+
+                <div className="social-share__row--links">
+                    <Link className='news-desktop__scan-share' id="copy__button">
+                        <lord-icon
+                            id="share__button"
+                            src="https://cdn.lordicon.com/cbigqefp.json"
+                            trigger="morph"
+                            state="morph-link"
+                            colors="primary:#fff,secondary:#b4b4b4">
+                        </lord-icon>Copy
+                    </Link>
+                    <EmailShareButton className='social-share__icons'><EmailIcon className='social-share__icons' /><h3 className='social-share__text'>Email</h3></EmailShareButton>
+                    <FacebookShareButton className='social-share__icons'><FacebookIcon className='social-share__icons' /><h3 className='social-share__text'>Facebook</h3></FacebookShareButton>
+                    <LinkedinShareButton className='social-share__icons'><LinkedinIcon className='social-share__icons' /><h3 className='social-share__text'>LinkedIn</h3></LinkedinShareButton>
+                </div>
+                <div className="social-share__row--links">
+                    <PinterestShareButton className='social-share__icons'><PinterestIcon className='social-share__icons' /><h3 className='social-share__text'>Pinterest</h3></PinterestShareButton>
+                    <RedditShareButton className='social-share__icons'><RedditIcon className='social-share__icons' /><h3 className='social-share__text'>Reddit</h3></RedditShareButton>
+                    <TwitterShareButton className='social-share__icons'><TwitterIcon className='social-share__icons' /><h3 className='social-share__text'>Twitter</h3></TwitterShareButton>
+                    <WhatsappShareButton className='social-share__icons'><WhatsappIcon className='social-share__icons' /><h3 className='social-share__text'>WhatsApp</h3></WhatsappShareButton>
                 </div>
             </section>
         </>
